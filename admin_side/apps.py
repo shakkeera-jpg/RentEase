4c38196b5c3fe233f822570c02bf8324bd1f94e1
Shakkeera <shakkeerawindow@gmail.com>
2026-03-24 14:15:25 +0530

    updated

--- admin_side/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdinSideConfig(AppConfig):
-    name = 'admin_side'
--- admin_side/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from .views import PendingVerificationListView, ApproveRejectVerificationView
-
-urlpatterns = [
-    path('pending/', PendingVerificationListView.as_view(), name='admin-pending-list'),
-    path('action/<int:profile_id>/', ApproveRejectVerificationView.as_view(), name='admin-verify-action'),
-]
